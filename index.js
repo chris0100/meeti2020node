6ff375129580aa4b80b5902dc1e62f719e5c5960
Alexis Rodriguez <alexis.rodriguez@correounivalle.edu.co>
2020-06-24 10:39:39 -0500

    arreglo final2

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.use('/', router());
 
 //Leer el host y el puerto
 const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 
 //agrega el puerto
